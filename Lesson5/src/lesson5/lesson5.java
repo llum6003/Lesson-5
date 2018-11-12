7837ea7971334404a0c7e14965b540b01b27bcc4
llum6003 <44827939+llum6003@users.noreply.github.com>
2018-11-12 04:43:53 -0500

    Add files via upload

---
+++ Lesson5/src/lesson5/lesson5.java
@@ -0,0 +1,44 @@
+package lesson5;
+
+public class lesson5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Prmitive type
+		byte num;
+    	
+    	num = 113;
+    	System.out.println(num);
+    	
+    	//short
+    	short num1;
+    	
+    	num1 = 150;
+    	System.out.println(num1);
+    	
+    	//long
+    	long num2 = -12332252626L;
+    	System.out.println(num2);
+    	
+    	//double
+    	double num3 = -42937737.9d;
+    	System.out.println(num3);
+    	
+    	//float
+    	float num4 = 19.98f;
+    	System.out.println(num4);
+    	
+    	//boolean
+    	boolean b = false;
+    	System.out.println(b);
+    	
+    	//character
+    	char ch = 'Z';
+    	System.out.println(ch);
+    	
+ 
+    	
+    	
+	}
+
+}
